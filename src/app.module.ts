0702815301b4612aeb81395f202bc740d93d7e90
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-04-04 16:34:32 -0300

    Creating the fisrt module with cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceptsManualModule } from './concepts/concepts-manual.module';
+import { ConceptsAutomaticModule } from './concepts-automatic/concepts-automatic.module';
 
 @Module({
-  imports: [],
+  imports: [ConceptsManualModule, ConceptsAutomaticModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+ 
